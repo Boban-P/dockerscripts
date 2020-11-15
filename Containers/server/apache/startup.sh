e5d3a3b9ead4c8ec50f37c1bf09503c602e6418a
Boban Pulinchery <boban@ecodiver.in>
2020-11-15 09:08:11 +0530

    Apache lets encrypt, php & postfix mail system, application repository branch

--- Containers/server/apache/startup.sh
+++ Containers/server/apache/startup.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+#################
+# DEFAULT VALUES#
+#################
+LISTEN_PORT=${LISTEN_PORT:-80}
+LISTEN_SSLPORT=${LISTEN_SSLPORT:-443}
+SITE_NAME=${SITE_NAME:-localhost}
+# SITE_ALIAS=
+SITE_ADMIN=${SITE_ADMIN:-"webmaster@${SITE_NAME}"}
+SSL_ON=${SSL_ON:-0}
+ENABLE_MODE_balancer=${ENABLE_MODE_balancer:-0}
+export ENABLE_MODE_rewrite=${ENABLE_MODE_rewrite:-1}
+PHP_BALANCER_URL=${PHP_BALANCER_URL:-"127.0.0.1:5555"}
+PHP_ENVIRONMENT=${PHP_ENVIRONMENT:-"development"}
+CHILD_BALANCER_URL=${CHILD_BALANCER_URL:-"127.0.0.1:8080"}
+# DOCUMENT_ROOT=
+SITE_LOG_LEVEL=${SITE_LOG_LEVEL:-warn}
+SSL_ONLY=${SSL_ONLY:-0}
+# TRUSTED_PROXY=""
+# On | Off | EMail
+SERVER_SIGNATURE=${SERVER_SIGNATURE:-"Off"}
+# Full | OS | Minimal | Minor | Major | Prod
+SERVER_TOKEN=${SERVER_TOKEN:-"Prod"}
+# Set to one of:  On | Off | extended
+TRACE_ENABLE=${TRACE_ENABLE:-"Off"}
+# Set to one of: production | staging
+# ACME_ENABLE=0 \
+# ACME_PROVIDER=letsencrypt|letsencrypt-staging(default)|https://url.to.certificat/provider/path
+if [[ -n "${ACME_PROVIDER}" ]];then
+    ACME_ENABLE=1
+fi
+
 a2dissite 000-default >/dev/null
 
 sed -i -e "s/Listen 80/Listen ${LISTEN_PORT}/" -e "s/Listen 443/Listen ${LISTEN_SSLPORT}/" /etc/apache2/ports.conf
@@ -27,7 +58,7 @@ for line in $(env | grep -e '^ENABLE_MODE_'); do
 done
 
 if [[ "${SSL_ON:-0}" != "0" ]]; then
-    modules+=(ssl)
+    modules+=(ssl md)
 fi
 
 if [[ ${#modules[@]} -gt 0 ]]; then
@@ -47,6 +78,22 @@ if [[ ${#confs[@]} -gt 0 ]]; then
     a2enconf "${confs[@]}" >/dev/null
 fi
 
+if [[ -n "${TRUSTED_PROXY}" ]]; then
+    TRUSTED_PROXY_STRING="RemoteIPTrustedProxy ${TRUSTED_PROXY}"
+fi
+
+# SECURITY
+# Set to one of:  Full | OS | Minimal | Minor | Major | Prod
+# ServerTokens ${SERVER_TOKEN}
+# Set to one of:  On | Off | EMail
+# ServerSignature ${SERVER_SIGNATURE}
+# Set to one of:  On | Off | extended
+# TraceEnable ${TRACE_ENABLE}
+sed -i -e 's/^ServerTokens .*$/ServerTokens '"${SERVER_TOKEN}/" \
+    -e 's/^ServerSignature .*$/ServerSignature '"${SERVER_SIGNATURE}/" \
+    -e 's/^TraceEnable .*$/TraceEnable '"${TRACE_ENABLE}/" \
+    /etc/apache2/conf-available/security.conf
+
 # A node balancer do not have document root enabled.
 # IF document root is enabled and php backend exists
 # then php node balancer is enabled.
@@ -59,28 +106,27 @@ if [[ "${ENABLE_MODE_balancer:-0}" == "1" ]]; then
     fi
 
     if [[ ( -n "${DOCUMENT_ROOT}" ) && ( -n "${PHP_BALANCER_URL}" )]]; then
-        BALANCER="
-                 RemoteIPHeader X-Forwarded-For
-     <Proxy \"balancer://phpcluster\">
-#PHPBALANCER                     BalancerMember \"fcgi://BALANCER_URL\"
-     </Proxy>
-     # ProxyPassMatch not working with balancer.
-     # https://stackoverflow.com/a/41339419
-     #ProxyPassMatch ^/(.*\.php)$ balancer://phpcluster${DOCUMENT_ROOT%/}/\$1
-     <FilesMatch \.php$>
-                 ${envset}
-                 SetEnv HTTPS on
-                 SetHandler \"proxy:balancer://phpcluster\"
-     </FilesMatch>
+        BALANCER="     RemoteIPHeader X-Forwarded-For
+                 ${TRUSTED_PROXY_STRING}
+    <Proxy \"balancer://phpcluster\">
+#PHPBALANCER        BalancerMember \"fcgi://BALANCER_URL\"
+    </Proxy>
+    # ProxyPassMatch not working with balancer.
+    # https://stackoverflow.com/a/41339419
+    #ProxyPassMatch ^/(.*\.php)$ balancer://phpcluster${DOCUMENT_ROOT%/}/\$1
+    <FilesMatch \.php$>
+        ${envset}
+        SetEnv HTTPS on
+        SetHandler \"proxy:balancer://phpcluster\"
+    </FilesMatch>
 "
     else
-        BALANCER="
-            RemoteIPHeader X-Client-IP
-     <Proxy \"balancer://childcluster\">
+        BALANCER="    RemoteIPHeader X-Client-IP
+    <Proxy \"balancer://childcluster\">
 #CHILDBALANCER           BalancerMember \"http://BALANCER_URL\"
-     </Proxy>
-     ProxyPass        \"/\" \"balancer://childcluster/\"
-     ProxyPassReverse \"/\" \"balancer://childcluster/\"
+    </Proxy>
+    ProxyPass        \"/\" \"balancer://childcluster/\"
+    ProxyPassReverse \"/\" \"balancer://childcluster/\"
 "
     fi
 fi
@@ -89,32 +135,29 @@ if [[ -n "${DOCUMENT_ROOT}" ]]; then
     DOCUMENT_ROOT="
     DocumentRoot \"${DOCUMENT_ROOT}\"
     <Directory \"${DOCUMENT_ROOT}\">
-                Options Indexes FollowSymLinks MultiViews
-                AllowOverride All
-                Require all granted
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Require all granted
     </Directory>
 "
 fi
 
 if [[ -n "${SITE_ALIAS}" ]]; then
     SITE_ALIAS="ServerAlias ${SITE_ALIAS}"
+else
+    SITE_ALIAS='# ServerAlias'
 fi
 
 # VirtualHost skeleton
 
-VHOST_CONF="
-ServerName ${SITE_NAME}
-${SITE_ALIAS}
-ServerAdmin ${SITE_ADMIN}
-
+VHOST_CONF="    ServerName ${SITE_NAME}
+    ${SITE_ALIAS}
+    ServerAdmin ${SITE_ADMIN}
 ${DOCUMENT_ROOT}
 ${BALANCER}
-
-LogLevel ${SITE_LOG_LEVEL}
-ErrorLog \${APACHE_LOG_DIR}/error.log
-CustomLog \${APACHE_LOG_DIR}/access.log combined
-
-"
+    LogLevel ${SITE_LOG_LEVEL}
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined"
 
 hostfile="
 <VirtualHost *:${LISTEN_PORT}>
@@ -123,12 +166,47 @@ ${VHOST_CONF}
 "
 
 if [[ "${SSL_ON:-0}" != "0" ]]; then
-    if [[ ! -f /etc/apache2/ssl/"${SITE_NAME}.key" ]]; then
-        mkdir -p /etc/apache2/ssl
-        cp /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/apache2/ssl/"${SITE_NAME}.pem"
-        cp /etc/ssl/private/ssl-cert-snakeoil.key /etc/apache2/ssl/"${SITE_NAME}.key"
+
+    if [[ -n "${ACME_ENABLE//0/}" ]]; then
+        case "${ACME_PROVIDER:-letsencrypt-staging}" in
+            letsencrypt|default)
+                ;;
+            letsencrypt-staging)
+                CERTIFICATE_AUTHORITY="MDCertificateAuthority https://acme-staging-v02.api.letsencrypt.org/directory"
+                ;;
+            *)
+                CERTIFICATE_AUTHORITY="MDCertificateAuthority ${ACME_PROVIDER}"
+                ;;
+        esac
+        ACME_CONF="ServerAdmin ${SITE_ADMIN}
+MDCertificateAgreement accepted
+MDomain ${SITE_NAME}
+MDPrivateKeys RSA 4096
+${CERTIFICATE_AUTHORITY:-"# "}
+MDStoreDir /etc/apache2/md
+
+## Turn on OCSP Stapling ##
+SSLUseStapling On
+SSLStaplingCache \"shmcb:logs/ssl_stapling\"
+
+## md status
+<Location \"/md-status\">
+    SetHandler md-status
+</Location>
+"
+        SSLEngine_CONF="       SSLEngine on"
+    else
+        if [[ (! -f "/etc/apache2/ssl/${SITE_NAME}.pem") || (! -f "/etc/apache2/ssl/${SITE_NAME}.key") ]];then
+            mkdir -p /etc/apache2/ssl
+            cp /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/apache2/ssl/"${SITE_NAME}.pem"
+            cp /etc/ssl/private/ssl-cert-snakeoil.key /etc/apache2/ssl/"${SITE_NAME}.key"
+        fi
+        SSLEngine_CONF="
+     SSLEngine on
+     SSLCertificateFile	/etc/apache2/ssl/${SITE_NAME}.pem
+     SSLCertificateKeyFile   /etc/apache2/ssl/${SITE_NAME}.key
+       "
     fi
-    
     if [[ "${SSL_ONLY:-0}" != "0" ]]; then
         hostfile="
 <VirtualHost *:${LISTEN_PORT}>
@@ -139,16 +217,14 @@ if [[ "${SSL_ON:-0}" != "0" ]]; then
 </VirtualHost>
 "
     fi
-    
-    hostfile=${hostfile}"
+
+    hostfile="${ACME_CONF}${hostfile}
 <VirtualHost *:${LISTEN_SSLPORT}>
 ${VHOST_CONF}
-        SSLEngine on
-        SSLCertificateFile	/etc/apache2/ssl/${SITE_NAME}.pem
-        SSLCertificateKeyFile   /etc/apache2/ssl/${SITE_NAME}.key
-        <FilesMatch \"\\.(cgi|shtml|phtml|php)$\">
-	            SSLOptions +StdEnvVars
-        </FilesMatch>
+${SSLEngine_CONF}
+    <FilesMatch \"\\.(cgi|shtml|phtml|php)$\">
+        SSLOptions +StdEnvVars
+    </FilesMatch>
 </VirtualHost>
 "
 fi
--- Packages/projects/webserver/bin/startall.sh
+++ Packages/projects/webserver/bin/startall.sh
@@ -3,11 +3,17 @@
 
 # Start/stop all services.
 ip=""
+publicip=""
 args=()
 while [[ $# -gt 0 ]]; do
     case $1 in
         --ip)
             ip=${2}
+            shift
+            shift
+            ;;
+        --public)
+            publicip=${2}
             shift
             shift
             ;;
@@ -22,6 +28,10 @@ set -- "${args[@]}"
 
 if [[ -z "${ip}" ]]; then
     ip=172.17.0.1
+fi
+
+if [[ -z "${publicip}" ]]; then
+    publicip=${ip}
 fi
 
 
@@ -47,7 +57,7 @@ case "$1" in
         dnsmasq start "${ip}":53
         db start "${ip}":3306
         app start -d "${ip}" -m "${ip}" -n "${ip}" "${ip}":
-        web start "$(app url 0)" "${ip}":
-        balancer start "$(web url 0)" "${ip}"
+        web start "$(app url 0)" "${ip}": "${publicip}"
+        balancer start "$(web url 0)" "${publicip}"
         ;;
 esac
