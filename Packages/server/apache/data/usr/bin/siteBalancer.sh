b65c8300fd88cb01a564acf314c60a077d2fef5c
Boban Pulinchery <boban@ecodiver.in>
2021-01-14 20:07:32 +0530

    kubernets cluster node change update script

--- Packages/server/apache/data/usr/bin/siteBalancer.sh
+++ Packages/server/apache/data/usr/bin/siteBalancer.sh
@@ -4,6 +4,21 @@ show_help() {
 }
 # Add or remove child balancer
 
+args=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --no-update)
+            NO_UPDATE=1
+            ;;
+        *)
+            args+=("$1")
+            ;;
+    esac
+    shift
+done
+
+set -- "${args[@]}"
+
 name="$(sed 's/[^a-zA-Z0-9_]/_/g' <<<"${1}")"
 
 conf_file="$(find /etc/apache2/sites-enabled -name "??_${name}.conf")"
@@ -29,7 +44,16 @@ case "$2" in
         ;;
     empty)
         [[ $# -ne 2 ]] && >&2 show_help && exit 1
-        sed -i '/^[^#]+BalancerMember /d' "${conf_file}"
+        sed -i '/^[^#]\+BalancerMember /d' "${conf_file}"
+        ;;
+    reset)
+        [[ $# -lt 3 ]] && >&2 show_help && exit 1
+        conf_data="$(sed '/^[^#]\+BalancerMember /d' "${conf_file}")"
+        for url in "${@:3}"; do
+            conf_data="$(sed -e 's%\(.*\)#SITEBALANCER\(.*\)\(BalancerMember "http://\)\(BALANCER_URL\)\(.*\)%\1\3'"${url}"'\5\n\1#SITEBALANCER\2\3\4\5%' \
+            <<<"${conf_data}")"
+        done
+        cat <<<"${conf_data}" >"${conf_file}"
         ;;
     *)
         show_help
@@ -37,4 +61,6 @@ case "$2" in
         ;;
 esac
 
-apache2ctl graceful
+if [[ -z "${NO_UPDATE}" ]]; then
+    apache2ctl graceful
+fi
